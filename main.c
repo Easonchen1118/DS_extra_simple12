3779a814f6715018e367ab960271677956e3dd94
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-20 21:26:48 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//遞迴程式練習
+int func (int num, int ans) {
+    if (num == 0 || num == 1) {
+        ans = num + 1;
+    }
+    else {
+        int temp = num / 2;
+        ans = func(num-1, ans) + func(temp, ans);
+    }
+    return ans;
+}
+
+int main(){
+    int inpt;
+    scanf("%d", &inpt);
+    int oupt = 0;
+    oupt = func(inpt, oupt);
+    printf("%d\n", oupt);
+    return 0;
+}
